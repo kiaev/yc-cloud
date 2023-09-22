32242ad318d6eb3c907e3fa4b1ffdfc364bb3c3f
Kiaev Ivan <dapa_maru@mail.ru>
2023-09-22 23:39:10 +0600

    zone

--- dns-zone/dns-zone.tf
+++ dns-zone/dns-zone.tf
@@ -7,3 +7,19 @@ resource "yandex_dns_zone" "yc-courses" {
   public           = true
 }
 
+resource "yandex_dns_recordset" "yc-courses-record" {
+  zone_id = yandex_dns_zone.yc-courses.id
+  name    = "*.infra.skyfam.ru."
+  type    = "A"
+  ttl     = 600
+  data    = ["158.160.28.249"]
+}
+
+resource "yandex_dns_recordset" "yc-courses-acme-challenge" {
+  zone_id = yandex_dns_zone.yc-courses.id
+  name    = "_acme-challenge.infra.skyfam.ru."
+  type    = "CNAME"
+  ttl     = 600
+  data    = ["fpqrfnfc2lqn66mhndog.cm.yandexcloud.net."]
+}
+
