d24c8afc3959d4a44e4964e66f1900b2bf2a40fa
Kiaev Ivan <dapa_maru@mail.ru>
2023-10-09 23:08:39 +0600

    sec_group

--- sec_group/vpc_sec_groups.tf
+++ sec_group/vpc_sec_groups.tf
@@ -74,6 +74,40 @@ resource "yandex_vpc_security_group" "yc_k8s_ansible" {
     v4_cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    protocol       = "TCP"
+    description    = "Allow HTTPS"
+    v4_cidr_blocks = ["0.0.0.0/0"]
+    port           = 443
+  }
+
+  ingress {
+    protocol          = "Any"
+    description       = "Self security group"
+    predefined_target = "self_security_group"
+    from_port         = 0
+    to_port           = 65535
+  }
+
+  ingress {
+    protocol       = "Any"
+    v4_cidr_blocks = ["10.96.0.0/16", "10.112.0.0/16"]
+    from_port      = 0
+    to_port        = 65535
+  }
+
+  ingress {
+    protocol       = "TCP"
+    v4_cidr_blocks = ["198.18.235.0/24", "198.18.248.0/24"]
+    from_port      = 0
+    to_port        = 65535
+  }
+
+  ingress {
+    protocol       = "ICMP"
+    v4_cidr_blocks = ["10.0.0.0/8", "192.168.0.0/16", "172.16.0.0/12"]
+  }
+
   egress {
     description    = "Permit ANY"
     protocol       = "ANY"
